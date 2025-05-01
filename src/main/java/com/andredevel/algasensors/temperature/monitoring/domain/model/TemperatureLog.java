1886399869bd1fd4420d2cff1b9f78f053fd5994
Andre goncalves <andrevilanovagon@hotmail.com>
2025-04-30 22:20:00 -0300

    feat: add temperatureLog

---
+++ src/main/java/com/andredevel/algasensors/temperature/monitoring/domain/model/TemperatureLog.java
@@ -0,0 +1,33 @@
+package com.andredevel.algasensors.temperature.monitoring.domain.model;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class TemperatureLog {
+    @Id
+    @AttributeOverride(name = "value", column = @Column(name = "id", columnDefinition = "UUID"))
+    private TemperatureLogId id;
+    
+    @Column(name = "\"value\"")
+    private Double value;
+    private OffsetDateTime registeredAt;
+    
+    @Embedded
+    @AttributeOverride(name ="value", column = @Column(name = "sensor_id", columnDefinition = "BIGINT"))
+    private SensorId sensorId;
+}
